6c05c8e14a8b5244bf85c58ba9481be9b9d30700
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2022-08-27 12:25:01 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,19 @@
+public class Output {
+    public static void main(String args[]) {
+        // printing a line
+        System.out.print("Hello World");
+        // printing more lines
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        // we need use println function to get two different lines
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        // we can also use "\n" function to break or end a line
+        System.out.println("Hello World\nHello world");
+        System.out.println("Hello World\nHello world");
+        System.out.println("Hello World\nHello world");
+        System.out.println("Hello World\nHello world");
+
+    }
+}
